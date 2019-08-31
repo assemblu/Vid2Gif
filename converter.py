74c69103ba44627cf3ce5517d16aab6f38c58c1f
Emir <40762681+doeppler@users.noreply.github.com>
2019-08-31 09:21:33 +0200

    gifMaker
    Added gifMaker function and using it

--- converter.py
+++ converter.py
@@ -3,4 +3,20 @@ import os
 
 clip = os.path.abspath('vid.mp4')
 
-print(clip)
+def gifMaker(inputPath, targetFormat):
+    outputPath = os.path.splitext(inputPath)[0] + targetFormat
+
+    print(f'converting {inputPath} \nto {outputPath}')
+
+    reader = imageio.get_reader(inputPath)
+    fps = reader.get_meta_data()['fps']
+
+    writer = imageio.get_writer(outputPath, fps = fps)
+
+    for frames in reader:
+        writer.append_data(frames)
+        print(f'Frame {frames}')
+    print('Done!')
+    writer.close()
+
+gifMaker(clip, '.gif')
